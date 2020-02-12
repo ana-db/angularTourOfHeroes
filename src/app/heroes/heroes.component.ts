8169a1fc99e4439e3beeccc11c2a530f3acfedb7
Ana Diez <anadiezbravo@outlook.com>
2020-02-12 18:54:15 +0100

    6.3- Crear héroe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -58,6 +58,17 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
+  //función para añadir un nuevo héroe:
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+
 } //fin HeroesComponent
 
 
